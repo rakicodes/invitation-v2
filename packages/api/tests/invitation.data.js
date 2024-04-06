5b68ff848d5c1933c6bd4e688fd179d5172736ea
rakicodes <goraquel22@gmail.com>
2024-04-06 16:17:18 -0600

    clean data

---
+++ packages/api/tests/invitation.data.js
@@ -0,0 +1,44 @@
+const invitationAddTestData = {
+    message: "Hiii! Wanna go to Edmonton with me on May 2?",
+    messageImage:
+      "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExZW91d2Nyd2tjcHBpYndkNDFkaGNuNGhkYnUwMm1zN2M5dzdzbm43NSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/sRIY7gVfIV5AMwNbha/giphy.gif",
+    responseEffect: true,
+    response: "",
+    successImage:
+      "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmZuZHF2YWtnbmd3bGRvZ3ZkbXBjOGo5ZjRqZHprNHFzZ2EydHUxbSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/3q2PV2UDy0VYKUvZSk/giphy.gif",
+    successMessage: "Yayyyy!!! I'm excited!! :P",
+    failedImage:
+      "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExeTF6Y2ZrYzRzOTE1NTgxNnNud2Q4d3NlYXFiZW8ydjQ3eDVsYnNsMiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/6qqfYC0LyAvWYKd8iW/giphy.gif",
+    failedMessage: "awww :'(",
+    recepient: "NY",
+    isPublic: true,
+    backgroundColor: "#f3ccd9",
+    fontColor: "#fff9fb",
+    buttonBackgroundColor: "#b25674",
+    buttonFontColor: "#fff9fb",
+}
+
+const invitationEditTestData = {
+    message: "Hiii! Wanna go to Edmonton with me on May 2?",
+    messageImage:
+      "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExZW91d2Nyd2tjcHBpYndkNDFkaGNuNGhkYnUwMm1zN2M5dzdzbm43NSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/sRIY7gVfIV5AMwNbha/giphy.gif",
+    responseEffect: true,
+    response: "yes",
+    successImage:
+      "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmZuZHF2YWtnbmd3bGRvZ3ZkbXBjOGo5ZjRqZHprNHFzZ2EydHUxbSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/3q2PV2UDy0VYKUvZSk/giphy.gif",
+    successMessage: "Yayyyy!!! I'm excited!! :P",
+    failedImage:
+      "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExeTF6Y2ZrYzRzOTE1NTgxNnNud2Q4d3NlYXFiZW8ydjQ3eDVsYnNsMiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/6qqfYC0LyAvWYKd8iW/giphy.gif",
+    failedMessage: "awww :'(",
+    recepient: "NY",
+    isPublic: true,
+    backgroundColor: "#f3ccd9",
+    fontColor: "#fff9fb",
+    buttonBackgroundColor: "#b25674",
+    buttonFontColor: "#fff9fb",
+}
+
+module.exports = {
+    invitationAddTestData,
+    invitationEditTestData
+}
