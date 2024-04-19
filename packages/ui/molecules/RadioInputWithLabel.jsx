7a04f03065b8224ccafe24507fb6f7e21ae925cb
rakicodes <goraquel22@gmail.com>
2024-04-19 02:15:53 -0600

    add border to radio group

--- packages/ui/molecules/RadioInputWithLabel.jsx
+++ packages/ui/molecules/RadioInputWithLabel.jsx
@@ -11,7 +11,7 @@ const RadioInputWithLabel = ({
   details,
 }) => {
   return (
-    <div className="flex gap-x-1 flex-wrap">
+    <div className={checked ? "flex gap-x-1 flex-wrap rounded border-2 border-lightpink p-2" : "flex gap-x-1 flex-wrap p-2"}>
       <RadioInput type={type} name={name} value={value} checked={checked} />
       <Label label={label} />
       <Paragraph>{details}</Paragraph>
