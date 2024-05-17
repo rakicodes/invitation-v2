df7bb81c2fd4a662318bb2b2c84ad3f4f818d20c
rakicodes <goraquel22@gmail.com>
2024-05-17 11:52:10 -0600

    add homepage

--- packages/web/src/app/page.js
+++ packages/web/src/app/page.js
@@ -2,27 +2,20 @@
 
 import Header from "@ui/organisms/Header"
 import HomeTemplate from "@ui/templates/HomeTemplate"
-import { useState, useEffect } from "react"
+import { useRouter } from "next/navigation";
+import { getCookie } from "cookies-next";
 
 export default function Home() {
-  const [data, setData] = useState([])
-  useEffect(() => {
-    const fetchData = async () => {
-			try {
-				const res = await fetch(`http://localhost:3333/api/invitations`);
-				const data = await res.json();
-        console.log(data)
-				setData(data);
-			} catch (error) {
-				console.log("error", error);
-			}
-		};
-
-		fetchData();
-  }, [])
-
+  const router = useRouter()
+  const handleCTA = () => {
+    if (getCookie("session")) {
+      router.push("/u/create")
+    } else {
+      router.push("/u/login")
+    }
+  }
   return <>
       <Header />
-      <HomeTemplate invitations={data} />
+      <HomeTemplate handleCTA={handleCTA}/>
   </>;
 }
