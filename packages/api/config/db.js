870f0f9490f1294f5853f71f8364e5b5d945dd2b
rakicodes <goraquel22@gmail.com>
2024-05-22 16:55:30 -0600

    add console.log for env variables

--- packages/api/config/db.js
+++ packages/api/config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+  console.log(process.env.NODE_ENV, process.env.DB_PROD_STRING)
   try {
     await mongoose.connect(
       process.env.NODE_ENV === "dev"
